ac587123dc4ff709556de36435c9e14d02e8333a
Jake Fireside <labountypaul@icloud.com>
2021-02-14 19:29:43 -0600

    created navbar

---
+++ client/src/components/navbar/MenuItems.js
@@ -0,0 +1,27 @@
+ export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Videos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Subscription',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Upload',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Logout',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
