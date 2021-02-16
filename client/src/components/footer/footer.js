464763b74cac4230989a45bfb3595727aa4ac5eb
Jake Fireside <labountypaul@icloud.com>
2021-02-16 15:16:50 -0600

    create footer

---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import '../Navbar/Navbar.css'
+
+function Footer() {
+    return(
+        <div className='NavbarItems'>
+            <p>
+                &copy; 2021 YourView
+            </p>
+        </div>
+    )
+}
+
+export default Footer
