ca561e8f49d5ce83f001cbee3ca127d336444316
KobeHill68 <kobehill68@gmail.com>
2021-02-17 09:00:49 -0600

    still working on the upload page added new files to

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
 import "./App.css";
 import Footer from "./components/footer/footer";
-
+import UploadVideo from "./components/upload/UploadVideoPage";
+import DetailVideoPage from "./components/video/DetailVideoPage";
+import SubscriptionPage from "./components/subscription/SubscriptionPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <br/>
+      <Router>
+        <div>
+          {/* <Route exact path="/" component={(login, false)} />
+            <Route exact path="/signup" component={(signup, false)} /> */}
+          {/* <Route exact path="/homepage" component={(null)} /> */}
+          <Route exact path="/video/upload" component={UploadVideo} />
+          {/* <Route exact path="/video/:videoId" component={(DetailVideoPage, null)} /> */}
+          <Route exact path="/subscription" component={SubscriptionPage} />
+        </div>
+      </Router>
       <Footer />
     </div>
   );
