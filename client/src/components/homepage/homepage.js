ba980c19306f69d534745bb624f0c7ba6d5c3d0a
Jake Fireside <labountypaul@icloud.com>
2021-02-17 14:00:36 -0600

    update homepage layout

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -1,12 +1,109 @@
-import './homepage.css'
+import "./homepage.css";
 
-function Homepage () {
-    return(
-        <div className="body">
-            Here is just a test to see how this looks!
-            <img  className="image" src="https://via.placeholder.com/150" alt="test"></img>
+function Homepage() {
+  return (
+    <div className="box">
+        <div className="banner">
+            <h3> Recommended</h3>
         </div>
-    )
+      <div className="row">
+        <div className="col-md-12 imgbox">
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
