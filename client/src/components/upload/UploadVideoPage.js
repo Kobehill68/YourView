a635946f9f96f6b4966ff54bcce73ee92703e0e1
jhuynh24 <jhuynh1735@gmail.com>
2021-02-19 02:57:22 -0600

    starter code for uploadvideo

---
+++ client/src/components/upload/UploadVideoPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './upload.css';
+import Logo from '../../assets/images/YourView (2).jpg';
+
+
+render() {
+    return (
+      //uploadvideo HTML
+    );
+  }
+
+export default UploadVideoPage;
+
+
