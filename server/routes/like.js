887170e993c3763736d5a410f9366542f82546bd
KobeHill68 <kobehill68@gmail.com>
2021-02-19 13:01:09 -0600

    still working on the server routes

--- server/routes/like.js
+++ server/routes/like.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const { Like } = require("../models/Like");
 const { Dislike } = require("../models/Dislike");
 
-router.post("/getlikes", (req, res) => {
+router.post("/getLikes", (req, res) => {
     let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    } else {
+        variable = { commentId: req.body.commentId }
+    }
+
+    Like.find(variable)
+        .exec((err, likes) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, likes })
+        })
+})
+
+router.post("/getDislikes", (req, res) => {
+    let variable = {}
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    } else {
+        variable = { commentId: req.body.videoId }
+    }
+
+    Dislike.find(variable)
+        .exec((err, dislikes) => {
+            if (err) return res.status(400).send(err)
+            res.status(200).json({ success: true, dislikes })
+        })
 })
