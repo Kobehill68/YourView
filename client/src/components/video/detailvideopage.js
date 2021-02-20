57f1a38be9f6809b45fea25e71fafc29434c2d7b
Paul LaBounty <labountypaul@icloud.com>
2021-02-19 22:30:17 -0600

    video page and subscription page templates

---
+++ client/src/components/video/detailvideopage.js
@@ -0,0 +1,109 @@
+import "./detailvideopage.css";
+
+function DetailVideoPage() {
+  return (
+    <div className="box">
+      <div className="banner">
+        <h3> Video </h3>
+      </div>
+      <div className="row">
+        <div className="col-md-12 imgbox">
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+          <img
+            className="image"
+            src="https://via.placeholder.com/200"
+            alt="test"
+          ></img>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailVideoPage;
