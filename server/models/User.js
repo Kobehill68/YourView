fc53fa8c601dda04fbfd27487096444a6ba4eea2
KobeHill68 <kobehill68@gmail.com>
2021-02-22 12:08:51 -0600

    changing the code to work with cloudinary

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,71 @@
-const { model, Schema } = require('mongoose');
-// https://www.npmjs.com/package/passport-local-mongoose
-const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
-const User = new Schema({});
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            max: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        salt: String,
+        about: {
+            type: String
+        },
+        subscribers: {
+            type: Number,
+            default: 0
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        }
+    },
+    { timestamps: true }
+);
 
-User.plugin(passportLocalMongoose);
+userSchema
+    .virtual('password')
+    .set(function (password) {
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function () {
+        return this._password
+    });
 
+userSchema.method = {
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    encryptPassword: function (password) {
+        if (!password) return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex")
+        } catch (err) {
+            return "";
+        }
+    },
+    makeSalt: function () {
+        return Math.round(new Date().valueOf() * Math.random()) + "";
+    }
+}
 
-
-
-
-module.exports = model('User', User);
+module.exports = mongoose.model("User", userSchema);
