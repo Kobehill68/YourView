9571691fa5ba1054e0ca6fe508d93516af4798a9
Paul LaBounty <labountypaul@icloud.com>
2021-02-22 12:48:30 -0600

    signup page

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,3 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import SignupForm from "./signupform";
+import SuccessForm from "./successform";
+import "./form.css";
 
+const Signup = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className="form-container">
+        {!isSubmitted ? (
+          <SignupForm submitForm={submitForm} />
+        ) : (
+          <SuccessForm />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Signup;
---
+++ client/src/components/signup/successform.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./form.css"
+
+const SuccessForm = () => {
+  return (
+    <div className="form-content-right">
+      <div className="form-success">Signup completed!</div>
+    </div>
+  );
+};
+
+export default SuccessForm;
